a1bbb701c311ff0f7f3dc39a6c9f4c09db1ba47b
Iftach - Lulav <iftahnaf@gmail.com>
2022-10-06 13:43:38 +0000

    launching node with parameters

--- src/dynamics/setup.py
+++ src/dynamics/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+from glob import glob
+import os
 
 package_name = 'dynamics'
 
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
